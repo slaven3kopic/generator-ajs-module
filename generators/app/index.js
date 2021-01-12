5de76dcebbf1f36d2724daa7cd49d48535053a48
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2021-01-12 08:53:44 +0100

    destination and module as lowercase

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,7 +9,7 @@ module.exports = yeoman.extend({
         const prompts = [{
                 type: 'input',
                 name: 'name',
-                message: 'Your module name',
+                message: 'Your module name (e.g. Dashboard or Header)',
                 //Defaults to the project's folder name if the input is skipped
                 default: this.appname
             },
@@ -30,13 +30,14 @@ module.exports = yeoman.extend({
         app: function() {
             fs.readdirSync(__dirname + '/templates/_ajs').forEach(file => {
                 const regex = /_ajs/gi;
-                const moduleName = file.replace(regex, this.props.name);
-                const destination = this.props.name;
+                const nameLC = this.props.name.charAt(0).toLowerCase() + this.props.name.slice(1);
+                const moduleName = file.replace(regex, nameLC);
+                const destination = nameLC;
                 this.fs.copyTpl(
                     this.templatePath(`_ajs/${file}`),
                     this.destinationPath(`${destination}/${moduleName}`), {
                         name: this.props.name,
-                        nameLC: this.props.name.charAt(0).toLowerCase() + this.props.name.slice(1),
+                        nameLC: nameLC,
                         id: this.props.id
                     }
                 );
